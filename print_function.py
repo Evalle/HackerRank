3896da8f77d8d58728d95d21a475bbc10ad40dcb
evalle <shmarnev@gmail.com>
2016-04-04 17:54:37 +0200

    Add template for print_function.py exercise

---
+++ print_function.py
@@ -0,0 +1,14 @@
+# Read an integer N
+# Without using any string methods, try to print the following:
+# 1,2,3...N
+# Note that "....." represents the values in between.
+## Input Format 
+# The first line contains an integer NN.
+## Output Format 
+# Output the answer as explained in the task.
+# Sample input:
+# 3
+# Sample output:
+# 123
+
+ 
