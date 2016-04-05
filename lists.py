ba9f327bbb2f4352b48631de06a3c624103a6091
evalle <shmarnev@gmail.com>
2016-04-05 16:08:58 +0200

    Add template for lists.py task

---
+++ lists.py
@@ -0,0 +1,32 @@
+''' Task 
+Initialize your list (L = []) and follow the NN commands given over NN lines.
+
+Each command will be 11 of the 88 commands given above. The extend(LL) method will not be used. Each command will have its own value(s) separated by a space.
+
+Input Format
+
+The first line contains an integer, NN (the number of commands). 
+The NN subsequent lines each contain one of the 88 commands described above.
+
+Sample Input
+
+12
+insert 0 5
+insert 1 10
+insert 0 6
+print 
+remove 6
+append 9
+append 1
+sort 
+print
+pop
+reverse
+print
+Sample Output
+
+[6, 5, 10]
+[1, 5, 9, 10]
+[9, 5, 1]
+''' 
+
