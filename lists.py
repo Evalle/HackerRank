bfe289933b9436865496ec4def5688ac7dce799d
evalle <shmarnev@gmail.com>
2016-04-05 16:52:38 +0200

    Add final working code for lists task

--- lists.py
+++ lists.py
@@ -29,4 +29,27 @@ Sample Output
 [1, 5, 9, 10]
 [9, 5, 1]
 ''' 
-
+counter = input()
+l = list()
+while counter > 0:
+    command = raw_input().split(" ")
+    if 'insert' in command:
+        l.insert(int(command[1]), int(command[2]))
+    if 'print' in command:
+        print l
+    if 'remove' in command:
+        l.remove(int(command[1]))
+    if 'append' in command:
+        l.append(int(command[1]))
+    if 'sort' in command:
+        l.sort()
+    if 'pop' in command:
+        l.pop()
+    if 'reverse' in command:
+        l.reverse()
+    if 'count' in command:
+        l.count(int(command[1]))
+    if 'index' in command:
+        l.index(int(command[1]))
+    counter -=1
+# also it can be a good idea to have 'for i in xrange(counter)' so you don't need to use counter anymore
