90c6135c7d46d7e9d3e5394bf74675717df95c98
evalle <shmarnev@gmail.com>
2016-04-06 13:25:52 +0200

    Add template for tuples.py task

---
+++ tuples.py
@@ -0,0 +1,24 @@
+'''
+Task 
+You are given an integer, NN, on a single line. The next line contains NN space-separated integers. Create a tuple, TT, of those NN integers, then compute and print the result of hash(TT).
+
+Note: hash() is one of the functions in the __builtins__ module.
+
+Input Format
+
+The first line contains an integer, NN (the number of elements in the tuple). 
+The second line contains NN space-separated integers describing TT.
+
+Output Format
+
+Print the result of hash(TT).
+
+Sample Input
+
+2
+1 2
+Sample Output
+
+3713081631934410656
+'''
+
