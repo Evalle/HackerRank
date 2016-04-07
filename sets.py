35d379dadcebb0514a2426398ac322565aebb94c
evalle <shmarnev@gmail.com>
2016-04-07 16:11:29 +0200

    Add template for sets.py task

---
+++ sets.py
@@ -0,0 +1,27 @@
+'''
+Task 
+Given 22 sets of integers, MM and NN, print their symmetric difference in ascending order. The term symmetric difference indicates those values that exist in either MM or NN but do not exist in both.
+
+Input Format
+
+The first line of input contains an integer, MM. 
+The second line contains MM space-separated integers. 
+The third line contains an integer, NN. 
+The fourth line contains NN space-separated integers.
+
+Output Format
+
+Output the symmetric difference integers in ascending order, one per line.
+
+Sample Input
+
+4
+2 4 5 9
+4
+2 4 11 12
+Sample Output
+
+5
+9
+11
+12
