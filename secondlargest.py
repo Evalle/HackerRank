41e95b63e8e1f7b874a625198e38612cbf85a487
evalle <shmarnev@gmail.com>
2016-04-22 16:13:57 +0200

    Add task for secondlargest number

---
+++ secondlargest.py
@@ -0,0 +1,8 @@
+'''
+Input Format
+The first line contains NN. The second line contains an array AA[] of NN integers each separated by a space.
+
+Output Format
+Output the value of the second largest number.
+'''
+
