a466629dab4280f88c941ed03a610a2df59e8c50
evalle <shmarnev@gmail.com>
2016-04-22 23:03:15 +0200

    add solution for second largest number task

--- secondlargest.py
+++ secondlargest.py
@@ -5,4 +5,16 @@ The first line contains NN. The second line contains an array AA[] of NN integer
 Output Format
 Output the value of the second largest number.
 '''
-
+nterms = int(input())
+numbers = raw_input().split()
+l = list()
+newlist = list()
+for i in numbers:
+    l.append(int(i))
+# creating list of unique numbers
+for i in l:
+    if i not in newlist:
+        newlist.append(i)
+newlist.sort()
+newlist.remove(max(newlist))
+print newlist.pop()
