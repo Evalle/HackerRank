f9fdef96c7ed03fc180ab0cadba18f2a84c6e285
evalle <shmarnev@gmail.com>
2016-04-25 16:40:31 +0200

    Add find_a_string task

---
+++ find_a_string.py
@@ -0,0 +1,22 @@
+'''
+input Format
+
+The first line of input contains the original string. The next line contains the substring.
+
+Constraints
+
+1≤len(string)≤2001≤len(string)≤200 
+Each character in the string is an ascii character.
+
+Output Format
+
+Output the integer number indicating the total number of occurrences of the substring in the original string.
+
+Sample Input
+ABCDCDC
+CDC
+
+Sample Output
+2
+'''
+
