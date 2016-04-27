682489979a352feedee00193e7aaa797ffc1a4c6
evalle <shmarnev@gmail.com>
2016-04-27 15:00:43 +0200

    Add task for string validator

---
+++ string_validators.py
@@ -0,0 +1,33 @@
+'''
+Task
+
+You are given a string SS. 
+Your task is to find out if the string SS contains: alphanumeric characters, alphabetical characters, digits, lowercase and uppercase characters.
+
+Input Format
+
+A single line containing a string SS.
+
+Constraints
+
+0<len(S)<10000<len(S)<1000
+
+Output Format
+In the first line, print True if SS has any alphanumeric characters. Otherwise, print False. 
+In the second line, print True if SS has any alphabetical characters. Otherwise, print False. 
+In the third line, print True if SS has any digits. Otherwise, print False. 
+In the fourth line, print True if SS has any lowercase characters. Otherwise, print False. 
+In the fifth line, print True if SS has any uppercase characters. Otherwise, print False.
+
+Sample Input
+
+qA2
+
+Sample Output
+True
+True
+True
+True
+True
+'''
+
