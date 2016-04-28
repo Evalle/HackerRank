096d30df18717a6dc105abc767acee0a425a2c76
evalle <shmarnev@gmail.com>
2016-04-28 18:22:50 +0200

    Add another task from HackerRank

---
+++ capitalize.py
@@ -0,0 +1,22 @@
+'''
+You are given a string SS. Your task is to capitalize each word of SS.
+
+Input Format
+
+A single line of input containing the string, SS.
+
+Constraints
+
+0<len(S)<10000<len(S)<1000
+The string consists of alphanumeric characters and spaces.
+
+Output Format
+
+Print the capitalized string, SS.
+
+Sample Input
+hello world
+
+Sample Output
+Hello World
+'''
