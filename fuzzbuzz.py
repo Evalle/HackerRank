7b770542ace6d14a1fbf8326c36e38327b87957e
Evgeny Shmarnev <shmarnev@gmail.com>
2016-05-09 13:18:35 +0200

    Update fuzzbuzz.py

--- fuzzbuzz.py
+++ fuzzbuzz.py
@@ -1,5 +1,7 @@
 '''
-Write a Python program to print numbers from 1 to 100 except for multiples of 3 for which you should print "fuzz" instead, for multiples of 5 you should print 'buzz' instead and for multiples of both 3 and 5, you should print 'fuzzbuzz' instead.
+Write a Python program to print numbers from 1 to 100 except for multiples of 3 
+for which you should print "fuzz" instead, for multiples of 5 you should print 
+'buzz' instead and for multiples of both 3 and 5, you should print 'fuzzbuzz' instead.
 '''
 for i in xrange(1,101):
     if i % 15 == 0:
