b897b6136c61184e8c3b7fc404ce8c30eda866f9
evalle <shmarnev@gmail.com>
2016-05-12 17:34:40 +0200

    Add task for text_wrap

---
+++ text_wrap.py
@@ -0,0 +1,30 @@
+'''
+Task
+
+You are given a string  and width . 
+Your task is to wrap the string into a paragraph of width .
+
+Input Format
+
+The first line contains a string, . 
+The second line contains the width, .
+
+Output Format
+
+Print the text wrapped paragraph.
+
+Sample Input
+
+ABCDEFGHIJKLIMNOQRSTUVWXYZ
+4
+Sample Output
+
+ ABCD
+ EFGH
+ IJKL
+ IMNO
+ QRST
+ UVWX
+ YZ 
+ '''
+
