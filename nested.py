eabcc91cf41e8b0b8bec84a141e64cda1196871b
evalle <shmarnev@gmail.com>
2016-05-27 16:59:39 +0200

    Added some functions to nested.py

--- nested.py
+++ nested.py
@@ -1,11 +1,31 @@
 namelist = list()
 gradelist = list()
+numbers = list()
+newlist = list()
 
 counter = int(input())
+
 for i in range(counter):
-    name = namelist.append(input())
-    grade = gradelist.append(input())
+    name = namelist.append(input().strip())
+    grade = gradelist.append(input().strip())
 
 studentlist = [list(l) for l in zip(namelist,gradelist)]
-print(studentlist)
+
+for list in studentlist:
+     for element in list:
+         if element.isdigit():
+             numbers.append(int(element))
+
+for i in numbers:
+    if i not in newlist:
+        newlist.append(i)
+newlist.sort()
+newlist.remove(max(newlist))
+minimum = newlist.pop()
+
+for list in studentlist:
+     for element in list:
+         if minimum in list:
+             print(list)
+
 
