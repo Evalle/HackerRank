33100c80bff983990314dfbe5c61c59df5ac5439
evalle <shmarnev@gmail.com>
2016-08-31 11:15:51 +0200

    Add solution for string validators exercise

--- string_validators.py
+++ string_validators.py
@@ -31,3 +31,10 @@ True
 True
 '''
 
+string = input()
+
+print(any(c.isalnum() for c in string))
+print(any(c.isalpha() for c in string))
+print(any(c.isdigit() for c in string))
+print(any(c.islower() for c in string))
+print(any(c.isupper() for c in string))
